fa3bffd83d6face4e7b76511d19e7f5c12172161
Muhammad Umer <m.umer@vertexitsol.com>
2023-11-29 22:37:07 +0500

    After completing Get Api

--- Services/course.services.js
+++ Services/course.services.js
@@ -1,34 +1,61 @@
+
 const CourseModel = require("../models/course.model");
 
 class CourseService {
     static async createCourse(userId, courseName, creditHours, section) {
-        // Implement the logic to map courseName to subjectCode here
+    
         const subjectCode = this.getSubjectCodeFromCourseName(courseName);
         
         const courseData = new CourseModel({ userId, subjectCode, courseName, creditHours, section });
         return await courseData.save();
     }
+    
 
     static async getCoursesByUserId(userId) {
         try {
+            console.log('Fetching courses for userId:', userId);
+    
             const courses = await CourseModel.find({ userId });
+    
+            console.log('Courses fetched successfully:', courses);
+    
             return courses;
         } catch (error) {
+            console.error('Error fetching courses:', error);
             throw error;
         }
     }
+    
 
     static async updateCourseStatus(userId, courseName, status) {
         return await CourseModel.findOneAndUpdate({ userId, courseName }, { status }, { new: true });
     }
 
-    static async deleteCourse(userId, courseId) {
-        return await CourseModel.findOneAndRemove({ userId, courseId });
-    }
+    // static async deleteCourseByUserIdAndCourseId(userId, courseId) {
+    //     try {
+    //         console.log(`Deleting course with ID ${courseId} for user with ID ${userId}`);
+    //         const deletedCourse = await CourseModel.findOneAndRemove({ userId, courseId });
+    
+    //         if (!deletedCourse) {
+    //             throw new Error('Course not found for the specified user and course ID.');
+    //         }
+    
+    //         console.log('Course deleted successfully:', deletedCourse);
+    //         return deletedCourse;
+    //     } catch (error) {
+    //         console.error('Error deleting course:', error);
+    //         throw error;
+    //     }
+    // }
+    
+
+    // static async deleteCourse(userId, courseId) {
+    //     return await CourseModel.findOneAndRemove({ userId, courseId });
+    // }
 
     // Helper function to get subjectCode from courseName
     static getSubjectCodeFromCourseName(courseName) {
-        // Basic example logic to map courseName to subjectCode
+        
         switch (courseName.toLowerCase()) {
             case 'english':
                 return '111';
@@ -38,7 +65,8 @@ class CourseService {
                 return '333';
             case 'chemistry':
                 return '484';
-            // Add more cases as needed
+            case 'biology':
+                return '343';    
             default:
                 return '';
         }
--- models/course.model.js
+++ models/course.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { UserModel } = require('../models/user.model'); // Update the path accordingly
+const { UserModel } = require('../models/user.model'); 
 const { Schema } = mongoose;
 
 function generateRandomCourseID(subjectCode) {
